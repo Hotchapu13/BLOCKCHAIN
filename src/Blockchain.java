43a12fc6efcae85d6e9279c8d2fe6edca277c509
Nuwe Bryant Nyero <nuwebryant@gmail.com>
2024-06-26 13:16:23 +0300

    new commit

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
 
 public class Blockchain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
     public static void main(String[] args) {
-        Block firstBlock = new Block("I'm the first block", "0");
-        System.out.println("Hash for block 1: " + firstBlock.hash);
-
-        Block secondBlock = new Block("I'm the second block", firstBlock.hash);
-        System.out.println("Hash for block 2: " + secondBlock.hash);
-
-        Block thirdBlock = new Block("I'm the first block", secondBlock.hash);
-        System.out.println("Hash for block 3: " + thirdBlock.hash);
+        // Adding blocks to the blockchain arraylist
+        blockchain.add(new Block("I'm the first block", "0"));
+        blockchain.add(new Block("I'm the second block", blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("I'm the third block", blockchain.get(blockchain.size()-1).hash));
+        
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
 
     }
 }
